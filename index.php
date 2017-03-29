95d78d4c9e1f6ee62c739b1c83fa2380f5b4451b
sabbir-anwar <protik26@gmail.com>
2017-03-29 20:18:30 +0600

    index file

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <title>Intelligent Tourism Project</title>
+      <link rel="stylesheet" href="css/footer.css">
+      <link rel="stylesheet" href="css/responsive.css">
+      <link rel="stylesheet" type="text/css" href="css/custom.css">
+   </head>
+   <body>
+   
+   
+   
+   
+    <script type="text/javascript" src="jq/jquery.js"></script>
+   </body>
+</html>   
